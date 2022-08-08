150b29af1d4ed852b4b5adb2a6af2d8b011d9507
bukinoshita <bukinoshita@gmail.com>
2022-08-08 11:07:02 -0300

    fix: build

--- src/klotty.ts
+++ src/klotty.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { SendEmailData } from './interfaces';
-import { render } from 'react-email';
 
 const VERSION = '0.2.1';
 
@@ -36,10 +35,6 @@ export default class Klotty {
   async sendEmail(data: SendEmailData): Promise<AxiosResponse> {
     try {
       const path = `${this.baseUrl}/email`;
-
-      if (data.react) {
-        data.html = render(data.react);
-      }
 
       return this.request(path, { method: 'POST', data });
     } catch (error) {
