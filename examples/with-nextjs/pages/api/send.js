74a01e2206ee0deeea4577dad6cc023c9d5fcbc2
Zeno Rocha <zno.rocha@gmail.com>
2022-08-09 20:56:11 -0700

    render react component on the server-side

--- examples/with-nextjs/pages/api/send.js
+++ examples/with-nextjs/pages/api/send.js
@@ -1,15 +1,15 @@
+import EmailTemplate from '../../components/EmailTemplate';
 import { Klotty } from 'klotty';
+
 const klotty = new Klotty(process.env.KLOTTY_API_KEY);
 
 export default async function send(req, res) {
   try {
-    const { template } = JSON.parse(req.body);
-
-    const { data } = await klotty.sendEmail({
+    const data = await klotty.sendEmail({
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_TO,
       subject: "hello world",
-      html: template,
+      react: <EmailTemplate name="John" product="MyApp" />,
     });
 
     res.status(200).json(data);
