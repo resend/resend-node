d73a5dc6be970fedded8d0e0f2f796fc51749ebf
bukinoshita <bukinoshita@gmail.com>
2022-08-09 23:20:28 -0300

    feat: add react render support

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,10 @@
+import * as React from 'react';
+
 export interface SendEmailData {
   from: string;
   to: string;
   subject?: string;
   text?: string;
   html?: string;
+  react?: React.ReactElement;
 }
