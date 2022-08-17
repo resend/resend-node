60271f89a9a6bf561f0677f002dc11f80ca021ca
Zeno Rocha <zno.rocha@gmail.com>
2022-08-17 16:48:34 -0700

    add TypeScript example

---
+++ examples/with-nextjs-typescript/pages/api/send.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import EmailTemplate from '../../components/EmailTemplate';
+import { Klotty } from 'klotty';
+const klotty = new Klotty(process.env.KLOTTY_API_KEY);
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const data = await klotty.sendEmail({
+      from: process.env.EMAIL_FROM || '',
+      to: process.env.EMAIL_TO || '',
+      subject: "hello world",
+      react: <EmailTemplate firstName="John" product="MyApp" />,
+    });
+
+    res.status(200).json(data);
+  }
+  catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+}
