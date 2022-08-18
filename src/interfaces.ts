a51b4e4be32013ab52a4648fe87dcee475b1a366
Zeno Rocha <zno.rocha@gmail.com>
2022-08-17 22:56:06 -0700

    add support for bcc and cc parameters

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,6 +3,8 @@ import { ReactElement } from 'react';
 export interface SendEmailData {
   from: string;
   to: string;
+  bcc?: string;
+  cc?: string;
   subject?: string;
   text?: string;
   html?: string;
