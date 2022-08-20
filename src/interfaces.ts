58708c3897cbcbbd6efe7740c2cde20baf05d255
Zeno Rocha <zno.rocha@gmail.com>
2022-08-20 11:37:27 -0700

    add interface documentation

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,20 @@
 import { ReactElement } from 'react';
 
 export interface SendEmailData {
+  /** Sender email address. */
   from: string;
+  /** Recipient email address. Max 50. */
   to: string | string[];
+  /** Bcc recipient email address. Max 50. */
   bcc?: string | string[];
+  /** Cc recipient email address. Max 50. */
   cc?: string | string[];
+  /** Email subject. */
   subject?: string;
+  /** The HTML version of the message. */
   text?: string;
+  /** The plain text version of the message. */
   html?: string;
+  /** The React version of the message. */
   react?: ReactElement;
 }
