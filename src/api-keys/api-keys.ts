fa74de747766c6349126129c399090a44f83f07b
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-03-25 03:37:11 -0300

    feat: Add API keys

---
+++ src/api-keys/api-keys.ts
@@ -0,0 +1,18 @@
+import { Resend } from '../resend';
+import { CreateApiKeyOptions, CreateApiKeyRequestOptions } from './interfaces';
+
+export class ApiKeys {
+  constructor(private readonly resend: Resend) {}
+
+  async create(
+    payload: CreateApiKeyOptions,
+    options: CreateApiKeyRequestOptions = {},
+  ): Promise<void> {
+    const { data } = await this.resend.post('/api-keys', payload, options);
+    return data;
+  }
+
+  async remove(id: string) {
+    await this.resend.delete(`/api-keys/${id}`);
+  }
+}
