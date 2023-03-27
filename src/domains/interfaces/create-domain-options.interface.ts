1f6fc5b819e0df89c8a1bf31087c3023d142cc4b
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-03-27 14:49:56 -0300

    fix: Minor tweaks

--- src/domains/interfaces/create-domain-options.interface.ts
+++ src/domains/interfaces/create-domain-options.interface.ts
@@ -1,7 +1,10 @@
 import { PostOptions } from '../../common/interfaces';
 
+export type DomainRegion = 'us-east-1' | 'eu-west-1' | 'sa-east-1';
+
 export interface CreateDomainOptions {
-  name: string;
+  domain: string;
+  region?: DomainRegion;
 }
 
 export interface CreateDomainRequestOptions extends PostOptions {}
