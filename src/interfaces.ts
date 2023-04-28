ccba74c50bb678e4c13b952b6f715250fac2d82f
Ivo Ilić <3476570+ivoilic@users.noreply.github.com>
2023-04-27 21:12:57 -0400

    feat: Map error response type (#83)
    * Updated the error response type to be more explicit
    
    * Update src/interfaces.ts
    
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -53,13 +53,28 @@ export interface SendEmailRequest {
   tags?: Tag[];
 }
 
+export const RESEND_ERROR_CODES_BY_KEY = {
+  missing_required_fields: 422,
+  missing_api_key: 401,
+  invalid_api_Key: 403,
+  invalid_from_address: 403,
+  not_found: 404,
+  method_not_allowed: 405,
+  internal_server_error: 500
+} as const;
+
+type ValueOf<TObj> = TObj[keyof TObj];
+
+export type RESEND_ERROR_CODE_NUMBER = ValueOf<typeof RESEND_ERROR_CODES_BY_KEY>;
+export type RESEND_ERROR_CODE_KEY = keyof typeof RESEND_ERROR_CODES;
+
 interface EmailResponse extends Pick<SendEmailRequest, 'to' | 'from'> {
   id: string;
   created_at: string;
 }
 
 interface ErrorResponse {
-  error: { message: string; status: number; type: string };
+  error: { message: string; status: RESEND_ERROR_CODE_NUMBER; type: RESEND_ERROR_CODE_KEY };
 }
 
 export type SendEmailResponse = EmailResponse | ErrorResponse;
