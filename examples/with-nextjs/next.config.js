214c74262158a12d35600109d2dc137561d8f6da
Zeno Rocha <zno.rocha@gmail.com>
2023-05-21 09:32:37 -0700

    chore: Remove JS example in favor of TS

--- examples/with-nextjs/next.config.js
+++
@@ -1,7 +0,0 @@
-function throwError(envVar) {
-  throw `Abort: You need to define ${envVar} in the .env file.`
-}
-
-if (!process.env.RESEND_API_KEY) return throwError('RESEND_API_KEY');
-if (!process.env.EMAIL_FROM) return throwError('EMAIL_FROM');
-if (!process.env.EMAIL_TO) return throwError('EMAIL_TO');
