212e1112474d8f2a128ec43e502f12f50f023be0
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-05-24 23:24:13 -0300

    feat: Add URL file attachment support

--- src/emails/interfaces/create-email-options.interface.ts
+++ src/emails/interfaces/create-email-options.interface.ts
@@ -1,12 +1,16 @@
+import { ReactElement } from 'react';
 import { PostOptions } from '../../common/interfaces';
 
 export interface CreateEmailOptions {
+  attachments?: Attachment[];
   bcc?: string | string[];
   cc?: string | string[];
   from: string;
   html?: string;
+  react?: ReactElement;
   reply_to?: string | string[];
   subject: string;
+  tags?: Tag[];
   text?: string;
   to: string | string[];
 }
@@ -16,3 +20,11 @@ export interface CreateEmailRequestOptions extends PostOptions {}
 export interface CreateEmailResponse {
   id: string;
 }
+
+interface Attachment {
+  content?: string | Buffer;
+  filename?: string | false | undefined;
+  path?: string;
+}
+
+export type Tag = { name: string; value: string };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,58 +1,3 @@
-import { ReactElement } from 'react';
-
-export interface SendEmailData {
-  /** Sender email address. */
-  from: string;
-  /** Recipient email address. Max 50. */
-  to: string | string[];
-  /** Bcc recipient email address. Max 50. */
-  bcc?: string | string[];
-  /** Cc recipient email address. Max 50. */
-  cc?: string | string[];
-  /** Reply-to email address. Max 50. */
-  reply_to?: string | string[];
-  /** Email subject. */
-  subject: string;
-  /** The plain text version of the message. */
-  text?: string;
-  /** The HTML version of the message. */
-  html?: string;
-  /** The React version of the message. */
-  react?: ReactElement;
-  /** Attachments */
-  attachments?: Attachment[];
-  /** Email tags */
-  tags?: Tag[];
-}
-
-interface Attachment {
-  content?: string | Buffer;
-  filename?: string | false | undefined;
-}
-
-export interface SendEmailRequest {
-  /** Sender email address. */
-  from: string;
-  /** Recipient email address. Max 50. */
-  to: string | string[];
-  /** Bcc recipient email address. Max 50. */
-  bcc?: string | string[];
-  /** Cc recipient email address. Max 50. */
-  cc?: string | string[];
-  /** Reply-to email address. Max 50. */
-  reply_to?: string | string[];
-  /** Email subject. */
-  subject: string;
-  /** The HTML version of the message. */
-  text?: string;
-  /** The plain text version of the message. */
-  html?: string;
-  /** Attachments */
-  attachments?: Attachment[];
-  /** Email tags */
-  tags?: Tag[];
-}
-
 export const RESEND_ERROR_CODES_BY_KEY = {
   missing_required_fields: 422,
   missing_api_key: 401,
@@ -70,11 +15,6 @@ export type RESEND_ERROR_CODE_NUMBER = ValueOf<
 >;
 export type RESEND_ERROR_CODE_KEY = keyof typeof RESEND_ERROR_CODES_BY_KEY;
 
-interface EmailResponse extends Pick<SendEmailRequest, 'to' | 'from'> {
-  id: string;
-  created_at: string;
-}
-
 interface ErrorResponse {
   error: {
     message: string;
@@ -83,8 +23,6 @@ interface ErrorResponse {
   };
 }
 
-export type SendEmailResponse = EmailResponse | ErrorResponse;
-
-export { EmailResponse, ErrorResponse };
+export { ErrorResponse };
 
 export type Tag = { name: string; value: string };
