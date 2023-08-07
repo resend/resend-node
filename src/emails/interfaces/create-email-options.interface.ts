7d1a7edf23f40bbbfaa2a6dce38839dc76795a3f
Raul Sanchez <raulsanchez1024@gmail.com>
2023-08-07 06:48:58 -0600

    Removes type-fest

--- src/emails/interfaces/create-email-options.interface.ts
+++ src/emails/interfaces/create-email-options.interface.ts
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import { PostOptions } from '../../common/interfaces';
-import { RequireAtLeastOne } from 'type-fest';
 
-interface CreateEmailBaseOptions {
+type RequireAtLeastOne<T> = {
+  [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>
+}[keyof T]
+
+interface EmailRenderOptions {
+  html?:string;
+  react?: React.ReactElement | React.ReactNode | null;
+  text?: string;
+}
+
+interface CreateEmailBaseOptions extends EmailRenderOptions {
   attachments?: Attachment[];
   bcc?: string | string[];
   cc?: string | string[];
   from: string;
   headers?: Record<string, string>;
-  react?: React.ReactElement | React.ReactNode | null;
-  html?: string;
-  text?: string;
   reply_to?: string | string[];
   subject: string;
   tags?: Tag[];
   to: string | string[];
 }
 
-export type CreateEmailOptions = RequireAtLeastOne<
-  CreateEmailBaseOptions,
-  'react' | 'html' | 'text'
->;
+export type CreateEmailOptions = RequireAtLeastOne<EmailRenderOptions>;
 
 export interface CreateEmailRequestOptions extends PostOptions {}
 
