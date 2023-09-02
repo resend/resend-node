85f5356b12b91a9d5edcb1608d7cf17060301eb9
Zeno Rocha <zno.rocha@gmail.com>
2023-09-02 04:48:38 -0700

    fix: JSDoc broken URLs in type definitions #190

--- src/emails/interfaces/create-email-options.interface.ts
+++ src/emails/interfaces/create-email-options.interface.ts
@@ -6,73 +6,73 @@ interface CreateEmailBaseOptions {
   /**
    * Filename and content of attachments (max 40mb per email)
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   attachments?: Attachment[];
   /**
    * Blind carbon copy recipient email address. For multiple addresses, send as an array of strings.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   bcc?: string | string[];
   /**
    * Carbon copy recipient email address. For multiple addresses, send as an array of strings.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   cc?: string | string[];
   /**
    * Sender email address. To include a friendly name, use the format `"Your Name <sender@domain.com>"`
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   from: string;
   /**
    * Custom headers to add to the email.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   headers?: Record<string, string>;
   /**
    * The React component used to write the message.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   react?: React.ReactElement | React.ReactNode | null;
   /**
    * The HTML version of the message.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   html?: string;
   /**
    * The plain text version of the message.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   text?: string;
   /**
    * Reply-to email address. For multiple addresses, send as an array of strings.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   reply_to?: string | string[];
   /**
    * Email subject.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   subject: string;
   /**
    * Email tags
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   tags?: Tag[];
   /**
    * Recipient email address. For multiple addresses, send as an array of strings. Max 50.
    *
-   * @link https://resend.com/api-reference/emails/send-email#body-parameters
+   * @link https://resend.com/docs/api-reference/emails/send-email#body-parameters
    */
   to: string | string[];
 }
