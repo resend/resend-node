54c0fc15cddfc622fe47f24e750e2aa94158f4a8
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2024-02-09 22:31:00 -0300

    Update src/domains/interfaces/patch-domain.interface.ts

--- src/domains/interfaces/patch-domain.interface.ts
+++ src/domains/interfaces/patch-domain.interface.ts
@@ -4,7 +4,7 @@ import { Domain } from './domain';
 export interface PatchDomainsOptions {
   id: string;
   click_tracking?: boolean;
-  domain_tracking?: boolean;
+  open_tracking?: boolean;
 }
 
 export type PatchDomainsResponseSuccess = Pick<Domain, 'id'> & {
