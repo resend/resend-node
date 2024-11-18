cfad2ce46c5c7cd4ac3c33235d7bbcaafda8a0ca
Vitor Capretz <capretzvitor@gmail.com>
2024-11-18 13:37:02 -0300

    feat: Use dynamic import for `@react-email/render` (#438)

--- src/emails/emails.ts
+++ src/emails/emails.ts
@@ -1,4 +1,3 @@
-import { renderAsync } from '@react-email/render';
 import type * as React from 'react';
 import type { Resend } from '../resend';
 import type {
@@ -22,6 +21,7 @@ import type {
 } from './interfaces/update-email-options.interface';
 
 export class Emails {
+  private renderAsync?: (component: React.ReactElement) => Promise<string>;
   constructor(private readonly resend: Resend) {}
 
   async send(
@@ -36,9 +36,20 @@ export class Emails {
     options: CreateEmailRequestOptions = {},
   ): Promise<CreateEmailResponse> {
     if (payload.react) {
-      payload.html = await renderAsync(payload.react as React.ReactElement);
-      // biome-ignore lint/performance/noDelete: <explanation>
-      delete payload.react;
+      if (!this.renderAsync) {
+        try {
+          const { renderAsync } = await import('@react-email/render');
+          this.renderAsync = renderAsync;
+        } catch (error) {
+          throw new Error(
+            'Failed to render React component. Make sure to install `@react-email/render`',
+          );
+        }
+      }
+
+      payload.html = await this.renderAsync(
+        payload.react as React.ReactElement,
+      );
     }
 
     const data = await this.resend.post<CreateEmailResponseSuccess>(
