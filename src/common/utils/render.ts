361fbfd76bb08c961e2838814d1732f22b441b78
gabriel miranda <gabrielmfern@outlook.com>
2025-04-24 08:23:11 -0300

    add render function depending on @react-email/render and @react-email/components

---
+++ src/common/utils/render.ts
@@ -0,0 +1,23 @@
+let rawRender: typeof import("@react-email/render").render | undefined =
+	undefined;
+
+export const render = async (
+	element: React.ReactElement,
+	options?: import("@react-email/render").Options,
+) => {
+	if (!rawRender) {
+		try {
+			rawRender = (await import("@react-email/render")).render;
+		} catch (exception) {
+			try {
+				rawRender = (await import("@react-email/components")).render;
+			} catch (exception) {
+				throw new Error(
+					"Failed to render React component. Make sure to install `@react-email/render` or `@react-email/components`",
+				);
+			}
+		}
+	}
+
+	return rawRender(element, options);
+};
