1c603806c8e0016a5ebf79f647d04112e13879e3
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2025-07-08 13:11:56 -0300

    feat: add contact/topics support

---
+++ src/contacts/topics/interfaces/update-contact-topics.interface.ts
@@ -0,0 +1,24 @@
+import type { PatchOptions } from '../../../common/interfaces/patch-option.interface';
+import type { ErrorResponse } from '../../../interfaces';
+
+interface UpdateContactTopicsBaseOptions {
+  id?: string;
+  email?: string;
+}
+
+export interface UpdateContactTopicsOptions
+  extends UpdateContactTopicsBaseOptions {
+  opt_in?: string[];
+  opt_out?: string[];
+}
+
+export interface UpdateContactTopicsRequestOptions extends PatchOptions {}
+
+export interface UpdateContactTopicsResponseSuccess {
+  id: string;
+}
+
+export interface UpdateContactTopicsResponse {
+  data: UpdateContactTopicsResponseSuccess | null;
+  error: ErrorResponse | null;
+}
