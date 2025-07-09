6dad0157bb659f12f89740055bb4a1aba29cda2d
Carolina de Moraes Josephik <32900257+CarolinaMoraes@users.noreply.github.com>
2025-07-09 11:03:22 -0300

    feat: add contact topics endpoints to node sdk (#545)
    Co-authored-by: Alexandre Cisneiros <Cisneiros@users.noreply.github.com>

---
+++ src/contacts/topics/contact-topics.ts
@@ -0,0 +1,61 @@
+import type { Resend } from '../../resend';
+import type {
+  GetContactTopicsOptions,
+  GetContactTopicsResponse,
+  GetContactTopicsResponseSuccess,
+} from './interfaces/get-contact-topics.interface';
+import type {
+  UpdateContactTopicsOptions,
+  UpdateContactTopicsResponse,
+  UpdateContactTopicsResponseSuccess,
+} from './interfaces/update-contact-topics.interface';
+
+export class ContactTopics {
+  constructor(private readonly resend: Resend) {}
+
+  async update(
+    payload: UpdateContactTopicsOptions,
+  ): Promise<UpdateContactTopicsResponse> {
+    if (!payload.id && !payload.email) {
+      return {
+        data: null,
+        error: {
+          message: 'Missing `id` or `email` field.',
+          name: 'missing_required_field',
+        },
+      };
+    }
+
+    const identifier = payload.email ? payload.email : payload.id;
+    const data = await this.resend.patch<UpdateContactTopicsResponseSuccess>(
+      `/contacts/${identifier}/topics`,
+      {
+        opt_in: payload.opt_in,
+        opt_out: payload.opt_out,
+      },
+    );
+
+    return data;
+  }
+
+  async get(
+    options: GetContactTopicsOptions,
+  ): Promise<GetContactTopicsResponse> {
+    if (!options.id && !options.email) {
+      return {
+        data: null,
+        error: {
+          message: 'Missing `id` or `email` field.',
+          name: 'missing_required_field',
+        },
+      };
+    }
+
+    const identifier = options.email ? options.email : options.id;
+    const data = await this.resend.get<GetContactTopicsResponseSuccess>(
+      `/contacts/${identifier}/topics`,
+    );
+
+    return data;
+  }
+}
