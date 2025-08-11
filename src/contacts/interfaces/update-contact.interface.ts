9b2fc7ee0f17e56a034adc0f647783d4a85e2ab4
Gabriel Miranda <gabrielmfern@outlook.com>
2025-08-11 14:59:43 -0300

    feat: use typescript to ensure there's email or id for contacts.get

--- src/contacts/interfaces/update-contact.interface.ts
+++ src/contacts/interfaces/update-contact.interface.ts
@@ -1,17 +1,12 @@
 import type { ErrorResponse } from '../../interfaces';
-import type { Contact } from './contact';
+import type { Contact, SelectingField } from './contact';
 
-interface UpdateContactBaseOptions {
-  id?: string;
-  email?: string;
-}
-
-export interface UpdateContactOptions extends UpdateContactBaseOptions {
+export type UpdateContactOptions = {
   audienceId: string;
   unsubscribed?: boolean;
   firstName?: string;
   lastName?: string;
-}
+} & SelectingField;
 
 export type UpdateContactResponseSuccess = Pick<Contact, 'id'> & {
   object: 'contact';
