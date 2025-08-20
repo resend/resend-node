eb26c3a0b1c56f4d3cf7af629444549f1c1a9e4f
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2025-08-20 11:30:17 -0300

    feat: add template retrieval method

---
+++ src/templates/templates.ts
@@ -0,0 +1,49 @@
+import type { Resend } from '../resend';
+import type {
+  CreateTemplateOptions,
+  CreateTemplateResponse,
+  CreateTemplateResponseSuccess,
+} from './interfaces/create-template-options.interface';
+import type {
+  GetTemplateResponse,
+  GetTemplateResponseSuccess,
+} from './interfaces/get-template.interface';
+
+export class Templates {
+  private renderAsync?: (component: React.ReactElement) => Promise<string>;
+  constructor(private readonly resend: Resend) {}
+
+  async create(
+    payload: CreateTemplateOptions,
+  ): Promise<CreateTemplateResponse> {
+    if (payload.react) {
+      if (!this.renderAsync) {
+        try {
+          const { renderAsync } = await import('@react-email/render');
+          this.renderAsync = renderAsync;
+        } catch (error) {
+          throw new Error(
+            'Failed to render React component. Make sure to install `@react-email/render`',
+          );
+        }
+      }
+
+      payload.html = await this.renderAsync(
+        payload.react as React.ReactElement,
+      );
+    }
+
+    const data = await this.resend.post<CreateTemplateResponseSuccess>(
+      '/templates',
+      payload,
+    );
+    return data;
+  }
+
+  async get(identifier: string): Promise<GetTemplateResponse> {
+    const data = await this.resend.get<GetTemplateResponseSuccess>(
+      `/templates/${identifier}`,
+    );
+    return data;
+  }
+}
