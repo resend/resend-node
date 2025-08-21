0e5bea92c745433ed1201c437933a4f59b40a668
Gabriel Miranda <gabrielmfern@outlook.com>
2025-08-21 14:36:18 -0300

    use mjs for the vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    clearMocks: true,
-    restoreMocks: true,
     environment: 'node',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['vitest.setup.mjs'],
   },
 });
