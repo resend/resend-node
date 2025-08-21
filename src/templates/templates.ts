98dad1404a4a55e541088e853fbf10c53e0d04a1
Carolina de Moraes Josephik <32900257+CarolinaMoraes@users.noreply.github.com>
2025-08-21 15:34:16 -0300

    refactor: add chainable operations (#575)

--- src/templates/templates.ts
+++ src/templates/templates.ts
@@ -1,4 +1,5 @@
 import type { Resend } from '../resend';
+import { ChainableTemplateResult } from './chainable-template-result';
 import type {
   CreateTemplateOptions,
   CreateTemplateResponse,
@@ -25,7 +26,17 @@ export class Templates {
   private renderAsync?: (component: React.ReactElement) => Promise<string>;
   constructor(private readonly resend: Resend) {}
 
-  async create(
+  create(
+    payload: CreateTemplateOptions,
+  ): ChainableTemplateResult<CreateTemplateResponse> {
+    const createPromise = this.performCreate(payload);
+    return new ChainableTemplateResult(createPromise, this.publish.bind(this));
+  }
+  // This creation process is being done separately from the public create so that
+  // the user can chain the publish operation after the create operation. Otherwise, due
+  // to the async nature of the renderAsync, the return type would be
+  // Promise<ChainableTemplateResult<CreateTemplateResponse>> which wouldn't be chainable.
+  private async performCreate(
     payload: CreateTemplateOptions,
   ): Promise<CreateTemplateResponse> {
     if (payload.react) {
@@ -45,11 +56,10 @@ export class Templates {
       );
     }
 
-    const data = await this.resend.post<CreateTemplateResponseSuccess>(
+    return this.resend.post<CreateTemplateResponseSuccess>(
       '/templates',
       payload,
     );
-    return data;
   }
 
   async remove(identifier: string): Promise<RemoveTemplateResponse> {
@@ -66,11 +76,16 @@ export class Templates {
     return data;
   }
 
-  async duplicate(identifier: string): Promise<DuplicateTemplateResponse> {
-    const data = await this.resend.post<DuplicateTemplateResponseSuccess>(
+  duplicate(
+    identifier: string,
+  ): ChainableTemplateResult<DuplicateTemplateResponse> {
+    const promiseDuplicate = this.resend.post<DuplicateTemplateResponseSuccess>(
       `/templates/${identifier}/duplicate`,
     );
-    return data;
+    return new ChainableTemplateResult(
+      promiseDuplicate,
+      this.publish.bind(this),
+    );
   }
 
   async publish(identifier: string): Promise<PublishTemplateResponse> {
