56a848dc3e55a6bc52ed23cff239adf1884cc62e
Carolina de Moraes Josephik <32900257+CarolinaMoraes@users.noreply.github.com>
2025-08-22 10:06:49 -0300

    feat: add list method (#586)

--- src/common/interfaces/list-option.interface.ts
+++ src/common/interfaces/list-option.interface.ts
@@ -2,3 +2,13 @@ export interface List<T> {
   readonly object: 'list';
   data: T[];
 }
+
+export interface PaginatedList<T> extends List<T> {
+  has_more: boolean;
+}
+
+export interface PaginationOptions {
+  before?: string | null;
+  after?: string | null;
+  limit?: number | null;
+}
