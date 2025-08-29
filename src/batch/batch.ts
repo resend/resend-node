59c9e718d87ccec4f4047605a2782b488a965426
Pedro Gomes <pedro@resend.com>
2025-08-29 12:35:40 -0400

    fix: changes headers name to keep standard (#608)

--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -51,7 +51,7 @@ export class Batch {
       {
         ...options,
         headers: {
-          'x-batch-validation': options?.batchValidation ?? 'strict',
+          'x-resend-batch-validation': options?.batchValidation ?? 'strict',
           ...options?.headers,
         },
       },
