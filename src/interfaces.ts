6159fee30fb814f6d5870f59437f07191dc1fea9
Gabriel Miranda <gabrielmfern@outlook.com>
2025-08-29 09:10:25 -0300

    fix: links in js doc comments on rate limiting API

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -21,30 +21,32 @@ export const RESEND_ERROR_CODES_BY_KEY = {
 
 export type RESEND_ERROR_CODE_KEY = keyof typeof RESEND_ERROR_CODES_BY_KEY;
 
+export type RateLimitExceededErrorResponse = {
+  message: string;
+  name: Extract<RESEND_ERROR_CODE_KEY, 'rate_limit_exceeded'>;
+  /**
+   * Time in seconds.
+   */
+  retryAfter: number;
+};
+
 export type ErrorResponse =
   | {
-      message: string;
-      name: Exclude<RESEND_ERROR_CODE_KEY, 'rate_limit_exceeded'>;
-    }
-  | {
-      message: string;
-      name: Extract<RESEND_ERROR_CODE_KEY, 'rate_limit_exceeded'>;
-      /**
-       * Time in seconds.
-       */
-      retryAfter: number;
-    };
+    message: string;
+    name: Exclude<RESEND_ERROR_CODE_KEY, 'rate_limit_exceeded'>;
+  }
+  | RateLimitExceededErrorResponse;
 
 export type Response<Data> =
   | {
-      data: Data;
-      rateLimiting: RateLimit;
-      error: null;
-    }
+    data: Data;
+    rateLimiting: RateLimit;
+    error: null;
+  }
   | {
-      data: null;
-      rateLimiting: RateLimit | null;
-      error: ErrorResponse;
-    };
+    data: null;
+    rateLimiting: RateLimit | null;
+    error: ErrorResponse;
+  };
 
 export type Tag = { name: string; value: string };
