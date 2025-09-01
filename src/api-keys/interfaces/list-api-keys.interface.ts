0eb9014be564f21500fe0fb1a53ed314cf2cfade
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2025-09-01 18:25:22 -0300

    feat: add pagination support for list api-keys endpoint

--- src/api-keys/interfaces/list-api-keys.interface.ts
+++ src/api-keys/interfaces/list-api-keys.interface.ts
@@ -1,9 +1,13 @@
+import type { PaginationOptions } from '../../common/interfaces';
 import type { Response } from '../../interfaces';
 import type { ApiKey } from './api-key';
 
-export type ListApiKeysResponseSuccess = Pick<
-  ApiKey,
-  'name' | 'id' | 'created_at'
->[];
+export type ListApiKeysOptions = PaginationOptions;
+
+export type ListApiKeysResponseSuccess = {
+  object: 'list';
+  has_more: boolean;
+  data: Pick<ApiKey, 'name' | 'id' | 'created_at'>[];
+};
 
 export type ListApiKeysResponse = Response<ListApiKeysResponseSuccess>;
