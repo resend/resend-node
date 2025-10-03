e4567cddc184403f2f4dbfcb6ff2d204b2855292
Bu Kinoshita <bukinoshita@gmail.com>
2025-10-03 01:25:14 +0000

    fix: minor tweak

--- src/topics/topics.spec.ts
+++ src/topics/topics.spec.ts
@@ -130,14 +130,14 @@ describe('Topics', () => {
             id: 'b6d24b8e-af0b-4c3c-be0c-359bbd97381e',
             name: 'Newsletter',
             description: 'Weekly newsletter updates',
-            defaultSubscription: 'opt_in',
+            default_subscription: 'opt_in',
             created_at: '2023-04-07T23:13:52.669661+00:00',
           },
           {
             id: 'ac7503ac-e027-4aea-94b3-b0acd46f65f9',
             name: 'Product Updates',
             description: 'Product announcements and updates',
-            defaultSubscription: 'opt_out',
+            default_subscription: 'opt_out',
             created_at: '2023-04-07T23:13:20.417116+00:00',
           },
         ],
@@ -154,14 +154,14 @@ describe('Topics', () => {
     "data": [
       {
         "created_at": "2023-04-07T23:13:52.669661+00:00",
-        "defaultSubscription": "opt_in",
+        "default_subscription": "opt_in",
         "description": "Weekly newsletter updates",
         "id": "b6d24b8e-af0b-4c3c-be0c-359bbd97381e",
         "name": "Newsletter",
       },
       {
         "created_at": "2023-04-07T23:13:20.417116+00:00",
-        "defaultSubscription": "opt_out",
+        "default_subscription": "opt_out",
         "description": "Product announcements and updates",
         "id": "ac7503ac-e027-4aea-94b3-b0acd46f65f9",
         "name": "Product Updates",
