01c883ebc2837dfef5c49187320d1b0a41041f3a
Lucas da Costa <lucas@lucasfcosta.com>
2025-10-09 17:31:56 -0300

    feat: add inbound listing method (#668)

--- src/emails/receiving/receiving.ts
+++ src/emails/receiving/receiving.ts
@@ -1,8 +1,14 @@
+import { buildPaginationQuery } from '../../common/utils/build-pagination-query';
 import type { Resend } from '../../resend';
 import type {
   GetInboundEmailResponse,
   GetInboundEmailResponseSuccess,
 } from './interfaces/get-inbound-email.interface';
+import type {
+  ListInboundEmailsOptions,
+  ListInboundEmailsResponse,
+  ListInboundEmailsResponseSuccess,
+} from './interfaces/list-inbound-emails.interface';
 
 export class Receiving {
   constructor(private readonly resend: Resend) {}
@@ -11,6 +17,19 @@ export class Receiving {
     const data = await this.resend.get<GetInboundEmailResponseSuccess>(
       `/emails/receiving/${id}`,
     );
+
+    return data;
+  }
+
+  async list(
+    options: ListInboundEmailsOptions = {},
+  ): Promise<ListInboundEmailsResponse> {
+    const queryString = buildPaginationQuery(options);
+    const url = queryString
+      ? `/emails/receiving?${queryString}`
+      : '/emails/receiving';
+
+    const data = await this.resend.get<ListInboundEmailsResponseSuccess>(url);
 
     return data;
   }
