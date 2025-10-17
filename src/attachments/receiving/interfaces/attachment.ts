8631bd2f6accb2e6ce70265271b8c2153b952747
Lucas da Costa <lucas@lucasfcosta.com>
2025-10-17 10:55:17 -0300

    feat: avoid downloading attachments ourselves (#685)

--- src/attachments/receiving/interfaces/attachment.ts
+++ src/attachments/receiving/interfaces/attachment.ts
@@ -4,15 +4,6 @@ export interface InboundAttachment {
   content_type: string;
   content_disposition: 'inline' | 'attachment';
   content_id?: string;
-  content: string; // base64
-}
-
-export interface ApiInboundAttachment {
-  id: string;
-  filename?: string;
-  content_type: string;
-  content_disposition: 'inline' | 'attachment';
-  content_id?: string;
   download_url: string;
   expires_at: string;
 }
