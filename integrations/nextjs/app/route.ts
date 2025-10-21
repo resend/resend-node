0d32c63e37a245171541894771c0c90cd2245f75
gabriel miranda <gabrielmfern@outlook.com>
2025-10-21 14:18:01 -0300

    use another package.json

--- integrations/nextjs/app/route.ts
+++ integrations/nextjs/app/route.ts
@@ -1,7 +1,7 @@
-import { Resend } from '../../../';
+import { Resend } from 'resend';
 
 export function GET() {
-  new Resend(process.env.RESEND_API_KEY);
+  new Resend("");
 
   return new Response('Hello from this API route!', { status: 200 });
 }
