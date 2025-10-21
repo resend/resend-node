b8a33464b984152c1f9cc9417e191abbab38ead4
Gabriel Miranda <gabrielmfern@outlook.com>
2025-10-21 14:43:26 -0300

    fix: turbopack issues with optional @react-email/render (#696)

--- src/render.ts
+++ src/render.ts
@@ -1,18 +1,12 @@
-export function render(node: React.ReactNode) {
-  return new Promise<string>((resolve, reject) => {
-    // we don't use async here, because tsup transpiles it to
-    // using a generator syntax that, if used in conjunction
-    // with a bundler on the user's side, breaks these try-catch shenanigans
-    import('@react-email/render')
-      .then(({ render }) => {
-        resolve(render(node));
-      })
-      .catch(() => {
-        reject(
-          Error(
-            'Failed to render React component. Make sure to install `@react-email/render` or `@react-email/components`.',
-          ),
-        );
-      });
-  });
+export async function render(node: React.ReactNode) {
+  let render: typeof import('@react-email/render').render;
+  try {
+    ({ render } = await import('@react-email/render'));
+  } catch {
+    throw new Error(
+      'Failed to render React component. Make sure to install `@react-email/render` or `@react-email/components`.',
+    );
+  }
+
+  return render(node);
 }
