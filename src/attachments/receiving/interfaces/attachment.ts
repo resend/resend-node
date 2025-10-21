087035dca594fd901e2ec72d55e808d680f25431
Lucas Vieira <vieiralucas4@gmail.com>
2025-10-21 15:55:17 -0300

    feat: add size to attachment response

--- src/attachments/receiving/interfaces/attachment.ts
+++ src/attachments/receiving/interfaces/attachment.ts
@@ -1,6 +1,7 @@
 export interface InboundAttachment {
   id: string;
   filename?: string;
+  size: number;
   content_type: string;
   content_disposition: 'inline' | 'attachment';
   content_id?: string;
