5ea897e07e409635ebc90dfc605a52a19430a945
Alexandre Cisneiros <Cisneiros@users.noreply.github.com>
2025-10-29 17:07:51 +0000

    feat: add types for getting and setting contact property values (#730)

--- src/contacts/interfaces/get-contact.interface.ts
+++ src/contacts/interfaces/get-contact.interface.ts
@@ -7,12 +7,27 @@ export type GetContactOptions =
       audienceId?: string;
     } & SelectingField);
 
+type ContactPropertyValue =
+  | {
+      type: 'string';
+      value: string;
+    }
+  | {
+      type: 'number';
+      value: number;
+    };
+
+interface ContactProperties {
+  [key: string]: ContactPropertyValue;
+}
+
 export interface GetContactResponseSuccess
   extends Pick<
     Contact,
     'id' | 'email' | 'created_at' | 'first_name' | 'last_name' | 'unsubscribed'
   > {
   object: 'contact';
+  properties: ContactProperties;
 }
 
 export type GetContactResponse = Response<GetContactResponseSuccess>;
