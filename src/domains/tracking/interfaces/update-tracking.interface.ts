a5903cfc10f5df2a40a750eb07c7bb0def94b45b
mwoss <wos.mateusz16@gmail.com>
2026-03-30 12:17:36 +0200

    Remove is_active property

--- src/domains/tracking/interfaces/update-tracking.interface.ts
+++ src/domains/tracking/interfaces/update-tracking.interface.ts
@@ -2,7 +2,6 @@ import type { Response } from '../../../interfaces';
 import type { Tracking, TrackingRecord } from './tracking';
 
 export interface UpdateTrackingOptions {
-  is_active?: boolean;
   open_tracking?: boolean;
   click_tracking?: boolean;
 }
