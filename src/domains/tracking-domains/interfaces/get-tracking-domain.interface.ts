3f773e71500b605528610c3ac92d6ecb530416cc
rehanvandermerwe <rehan.vdm4@gmail.com>
2026-04-07 17:05:12 +0200

    Revert "feat: add support for custom tracking domains"
    This reverts commit 39a59f041d29d1b4633c6be16a555a041ba6a703.

--- src/domains/tracking-domains/interfaces/get-tracking-domain.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from '../../../interfaces';
-import type { TrackingDomain, TrackingDomainRecord } from './tracking-domain';
-
-export interface GetTrackingDomainResponseSuccess extends TrackingDomain {
-  records: TrackingDomainRecord[];
-}
-
-export type GetTrackingDomainResponse =
-  Response<GetTrackingDomainResponseSuccess>;
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export * from './contacts/interfaces';
 export * from './contacts/segments/interfaces';
 export * from './contacts/topics/interfaces';
 export * from './domains/interfaces';
-export * from './domains/tracking-domains/interfaces';
 export * from './emails/attachments/interfaces';
 export * from './emails/interfaces';
 export * from './emails/receiving/interfaces';
